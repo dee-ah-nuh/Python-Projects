088e5d48cf15b5ada26df9a756d9a759db21e895
Diana Valladares <96541076+dee-ah-nuh@users.noreply.github.com>
2022-07-31 09:53:24 -0500

    crash course book

---
+++ python-crash-course-program.py
@@ -0,0 +1,26 @@
+from datetime import MAXYEAR
+
+
+
+#first program
+print('hello_world!')
+
+#my name and some functions
+name = "diana valladares"
+print(name.title())
+maxy= 'maxy'
+maxy = maxy.title()
+print(maxy)
+print(maxy.upper())
+print(name.lower())
+
+#Using variables as strings 
+#we firt initialize a variable, then wse the function notation in print
+#finally we print the statement
+fullname = f"{maxy}s owner's name  is {name}"
+print(fullname)
+print(f"i miss you {maxy}")
+
+#lets do a function witihin a function i like to call it
+#this program is gonna be so long, but well get there i swear
+
