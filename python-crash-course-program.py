341efb9718b946a2c0fddfc6bc8b04e4d6b0d8d1
Diana Valladares <96541076+dee-ah-nuh@users.noreply.github.com>
2022-08-01 19:29:01 -0500

    Update python-crash-course-program.py

--- python-crash-course-program.py
+++ python-crash-course-program.py
@@ -24,3 +24,23 @@ print(f"i miss you {maxy}")
 #lets do a function witihin a function i like to call it
 #this program is gonna be so long, but well get there i swear
 
+print(f"Hello, my name is {name.title()}, my favorite pet in the whole wide world is not Noma, its {maxy}")
+
+print(f"{name} is about to start class")
+
+
+# WE INTERRUPT TO BRING A BRIEF MESSAGE:
+import sqlite3
+from sqlite3 import Error
+
+def create_connection():
+    """create a databa connection with a database that resides in the memory"""
+    conn=None;
+    try:
+        conn =sqlite3.connect('memory:')
+        print(sqlite3.version)
+    except Error as e:
+            print(e)
+    finally: 
+        if conn:
+            conn.close
